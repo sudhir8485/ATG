3f25740995032a5ce58781eaba6aeb78fb6da482
sudhir8485 <sudhirdhawade05@gmail.com>
2026-06-05 04:58:41 +0530

    revert: use plain-text passwords (NoOpPasswordEncoder)

--- src/main/java/com/nt/config/SecurityConfig.java
+++ src/main/java/com/nt/config/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -46,6 +46,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 }
